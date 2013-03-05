a7a17b3f66d3dcd6a26ea3f725f6669f945a6dd6
Thomas Eastman <tjeastman@gmail.com>
2013-03-04 20:11:43 -0800

    add emacs ido configuration

---
+++ emacs.d/init.el
@@ -0,0 +1,25 @@
+(setq inhibit-startup-message t)
+(setq initial-scratch-message nil)
+
+(if (fboundp 'scroll-bar-mode) (scroll-bar-mode -1))
+(if (fboundp 'tool-bar-mode) (tool-bar-mode -1))
+(if (fboundp 'menu-bar-mode) (menu-bar-mode -1))
+(if (fboundp 'blink-cursor-mode) (blink-cursor-mode -1))
+(if (fboundp 'fringe-mode) (fringe-mode 0))
+
+(defalias 'yes-or-no-p 'y-or-n-p)
+
+(setq confirm-nonexistent-file-or-buffer nil)
+(setq require-final-newline nil)
+(setq mouse-yank-at-point t)
+
+(delete-selection-mode t)
+(global-hl-line-mode 1)
+
+(require 'ido)
+(setq ido-enable-flex-matching t)
+(setq ido-create-new-buffer 'always)
+(setq ido-use-filename-at-point 'guess)
+(setq ido-ignore-extensions t)
+(ido-everywhere t)
+(ido-mode 1)
