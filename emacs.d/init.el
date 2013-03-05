9db2e21a2cd0a6e4a8cfd8e5b7866d52fd1813f2
Thomas Eastman <tjeastman@gmail.com>
2013-03-04 20:14:59 -0800

    add emacs smex configuration

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -23,3 +23,11 @@
 (setq ido-ignore-extensions t)
 (ido-everywhere t)
 (ido-mode 1)
+
+(require 'smex)
+(smex-initialize)
+(global-set-key (kbd "M-x") 'smex)
+(global-set-key (kbd "C-x C-m") 'smex)
+(global-set-key (kbd "C-c C-m") 'smex)
+(global-set-key (kbd "M-X") 'smex-major-mode-commands)
+(global-set-key (kbd "C-c C-c M-x") 'execute-extended-command)
