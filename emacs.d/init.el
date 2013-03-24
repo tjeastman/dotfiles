5e7b8e71a743b019713ed7f27a78cc7298a1fc67
Thomas Eastman <tjeastman@gmail.com>
2013-03-24 15:43:41 -0700

    enable emacs auto-completion flyspell workaround

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -137,6 +137,7 @@
 (require 'auto-complete-config)
 (add-to-list 'ac-dictionary-directories "~/.emacs.d/ac-dict")
 (ac-config-default)
+(ac-flyspell-workaround)
 
 (add-hook 'python-mode-hook
           (lambda ()
